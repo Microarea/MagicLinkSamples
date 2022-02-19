8da3f31012efa3d5fe432c0ee92d9ea3fa5aac4d
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2022-02-19 12:21:14 +0100

    created sample code

---
+++ YourFirstMagicLinkApp/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace YourFirstMagicLinkApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPost = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPost
+            // 
+            this.btnPost.Location = new System.Drawing.Point(254, 216);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(103, 23);
+            this.btnPost.TabIndex = 19;
+            this.btnPost.Text = "Post Contact";
+            this.btnPost.UseVisualStyleBackColor = true;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(15, 216);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 18;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Phone#";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Company";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Contact name";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(117, 170);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(240, 20);
+            this.txtPhone.TabIndex = 13;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(117, 111);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(240, 20);
+            this.txtAddress.TabIndex = 12;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(117, 62);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(240, 20);
+            this.txtCompanyName.TabIndex = 11;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(117, 13);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(240, 20);
+            this.txtContactName.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 271);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtContactName);
+            this.Name = "Form1";
+            this.Text = "MagicLink Sample";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtContactName;
+    }
+}
+
---
+++ YourFirstMagicLinkApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YourFirstMagicLinkApplication
+{
+    public partial class Form1 : Form
+    {
+        string server = ""; // the server where Mago is installed: "localhost" or some PC in the LAN
+        string instance = "Mago4"; // Mago4 is the default instance name, but it can be changed
+        string loginName = ""; // a valid user name
+        string password = ""; // its password
+        string companyName = ""; // name of the company, owner of the DB
+
+        string authenticationToken;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            using (MagoLoginManager.MicroareaLoginManagerSoapClient aLogMng = new MagoLoginManager.MicroareaLoginManagerSoapClient())
+            {
+                aLogMng.Endpoint.Address = new System.ServiceModel.EndpointAddress(string.Format("http://{0}/{1}/LoginManager/LoginManager.asmx", server, instance));
+
+                try
+                {
+                    int aRes = aLogMng.LoginCompact(ref loginName, ref companyName, password, "Your code here", true, out authenticationToken);
+
+                    if (aRes != 0) // 0 means no errors
+                        MessageBox.Show(string.Format("Error occurred, code: {0}", aRes));
+                    else
+                        MessageBox.Show("Connected!");
+                }
+                catch (Exception logExc)
+                {
+                    MessageBox.Show(string.Format("Exception occurred: {0}", logExc.Message));
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (authenticationToken != string.Empty)
+            {
+                using (MagoLoginManager.MicroareaLoginManagerSoapClient aLog = new MagoLoginManager.MicroareaLoginManagerSoapClient())
+                {
+                    aLog.Endpoint.Address = new System.ServiceModel.EndpointAddress(string.Format("http://{0}/{1}/LoginManager/LoginManager.asmx", server, instance));
+                    aLog.LogOff(authenticationToken);
+                }
+            }
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            // 1
+            string xmlData = $@"<?xml version='1.0'?>
+<maxs:Contacts xmlns:maxs='http://www.microarea.it/Schema/2004/Smart/ERP/Contacts/Contacts/Standard/DefaultLight.xsd' tbNamespace = 'Document.ERP.Contacts.Documents.Contacts' xTechProfile = 'DefaultLight' >
+    <maxs:Data>
+        <maxs:Contacts master='true'>
+            <maxs:CompanyName>{txtCompanyName.Text}</maxs:CompanyName>
+            <maxs:Address>{txtAddress.Text}</maxs:Address>
+            <maxs:Telephone1>{txtPhone.Text}</maxs:Telephone1>
+            <maxs:ContactPerson>{txtContactName.Text}</maxs:ContactPerson>
+        </maxs:Contacts>
+    </maxs:Data>
+</maxs:Contacts> ";
+
+            // 2
+            using (MagoTBService.TbServicesSoapClient aTbSvc = new MagoTBService.TbServicesSoapClient())
+            {
+                aTbSvc.Endpoint.Address = new System.ServiceModel.EndpointAddress(string.Format("http://localhost/{0}/TBServices/TBServices.asmx", instance));
+                string aResult = string.Empty;
+
+                // 3
+                try
+                {
+                    // 4
+                    if (aTbSvc.SetData(authenticationToken, xmlData, System.DateTime.Now, 0, false, out aResult))
+                        MessageBox.Show("Posted!");
+                    else
+                        MessageBox.Show("Not posted, some error occurred!");
+                }
+                catch (Exception tbExc)
+                {
+                    MessageBox.Show(string.Format("Exception occurred: {0}", tbExc.Message));
+                }
+            }
+
+        }
+    }
+}
